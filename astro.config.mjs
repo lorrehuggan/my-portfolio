9921bd2286188b678f74c1c502608dc3506d3ad2
lorre <lorre.huggan@protonmail.com>
2023-02-25 06:29:06 +0000

    chore: add astro vercel intergration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,23 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    config: {
-      applyBaseStyles: false
-    }
-  }), mdx()]
+  integrations: [
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    mdx(),
+  ],
+  output: 'static',
+  adapter: vercel(),
 });
