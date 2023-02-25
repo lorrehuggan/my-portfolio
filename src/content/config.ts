8aef2f12b7d8eedea894ad1e959ed3b0c4f0fd99
lorre <lorre.huggan@protonmail.com>
2023-02-25 01:27:11 +0000

    chore: move posts to astro contents

---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+import { z, defineCollection } from 'astro:content';
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    published: z.boolean(),
+    publishedDate: z.date(),
+    excerpt: z.string(),
+    author: z.string().default('Lorre Huggan'),
+    footnote: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
